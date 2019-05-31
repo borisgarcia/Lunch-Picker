67ac48ab672fd59ccd37592fcabf0186a874bf5e
Boris Garcia <brgp@live.com>
2019-05-31 10:47:10 -0600

    Backend Routes

---
+++ backend/routes/restaurants.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const router = express.Router();
+let restaurants = require("../database");
+
+router.get("/all", async (req, res) => {
+  try {
+    res.status(200).json({
+      data: restaurants
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "Can't get data",
+      err
+    });
+  }
+});
+
+router.get("/SortedByName", async (req, res) => {
+  try {
+    let restaurant = restaurants.sort(function(a,b){
+      return a.name.localeCompare(b.name);
+    });
+    res.status(200).json({
+      data: restaurant
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "Can't get data",
+      err
+    });
+  }
+});
+router.get("/SortedByCuisine", async (req, res) => {
+  try {
+    let restaurant = restaurants.sort(function(a,b){
+      return a.cuisine.localeCompare(b.cuisine);
+    });
+    res.status(200).json({
+      data: restaurant
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "Can't get data",
+      err
+    });
+  }
+});
+router.get("/SortedByRating", async (req, res) => {
+  try {
+    let restaurant = restaurants.sort(function(a,b){
+      return b.rating - a.rating;
+    });
+    res.status(200).json({
+      data: restaurant
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "Can't get data",
+      err
+    });
+  }
+});
+
+
+router.get("/:cuisine", async (req, res) => {
+  let { cuisine } = req.params;
+  try {
+    let restaurant = restaurants.find(restaurant => restaurant.cuisine === cuisine);
+    res.status(200).json({
+      data: restaurant
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "Can't get data",
+      err
+    });
+  }
+});
+
+module.exports = router;
